6bc2524008989e952d7afe84c615456f6335daa6
Eugene <ev.klit@gmail.com>
2019-10-31 21:15:52 +0300

    add lesson 4

---
+++ lesson_4/lib/route.rb
@@ -0,0 +1,26 @@
+class Route
+  attr_reader :stations, :name
+
+  def initialize(name, first_station, last_station)
+    @name = name
+    @stations = [first_station, last_station]
+  end
+
+  def to_s
+    @name
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    @stations.delete(station) if @stations.first != station && @stations.last != station
+  end
+
+  def show_stations
+    @stations.each_with_index do |station, index|
+      puts "№#{index + 1}: #{station}"
+    end
+  end
+end
